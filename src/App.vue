6e1d5654be79043cfd011a57ca32a2ed8f38d636
Xander Stoffels <xander.s@outlook.com>
2024-06-02 03:15:01 +0900

    Remove mobile banner

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
   <main>
     <RouterView />
   </main>
-  <MobileNavigationBar class="fixed bottom-0 left-0 border-t border-zinc-200 sm:hidden" />
+  <!-- <MobileNavigationBar class="fixed bottom-0 left-0 border-t border-zinc-200 sm:hidden" /> -->
 </template>
 
 <script setup lang="ts">
-import MobileNavigationBar from './components/MobileNavigationBar.vue';
+// import MobileNavigationBar from './components/MobileNavigationBar.vue';
 import NavigationBar from './components/NavigationBar.vue'
 </script>
 
